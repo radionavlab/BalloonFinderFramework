90e888b04acfb781f81ee85a74ce5dce1edebc91
tuckerhaydon <tuckerhaydon@gmail.com>
2018-03-29 15:53:47 -0700

    Removed unneccesary GPS stuff

--- src/callback.cc
+++ src/callback.cc
@@ -7,11 +7,9 @@
 
 #include "callback.h"
 #include "sensorParams.h"
-#include "gps.h"
 
 const std::vector<BalloonInfo> processImage(const cv::Mat& img) {
     /* Sensor params in: sensorParams */
-    /* GPS solution in: gpsSolution */
 
     /* 
     * YOUR CODE GOES HERE
